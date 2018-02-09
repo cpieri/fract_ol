8004b5cf1c28a2b5aebe165280a1efba3fd0db43
Clement PIERI <cpieri@e2r7p13.42.fr>
2018-02-09 09:44:42 +0100

    check_arg

--- src/main.c
+++ src/main.c
@@ -6,16 +6,31 @@
 /*   By: cpieri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 09:13:45 by cpieri            #+#    #+#             */
-/*   Updated: 2018/02/09 09:17:01 by cpieri           ###   ########.fr       */
+/*   Updated: 2018/02/09 09:43:24 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "fractol.h"
 
-int		main(int ac, char **av)
+static void		print_usage(void)
 {
-	ac = 0;
-	av = NULL;
+	ft_putendl("Usage : ./fractol [julia / mandelbrot]");
+	exit(0);
+}
+
+static void		check_arg(char **av)
+{
+	if (ft_strcmp("mandelbrot", av[1]) != 0 && ft_strcmp("julia", av[1]) != 0)
+		print_usage();
+	else
+		return ;
+}
+
+int				main(int ac, char **av)
+{
+	if (ac != 2)
+		print_usage();
+	check_arg(av);
 	return (0);
 }
